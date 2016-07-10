78ae09d22b53cd8eff5b0b9b103a270172668208
wangzhiyu0471 <447839120@qq.com>
2016-07-10 09:46:24 +0800

    v0.2

--- data/js/switch.js
+++ data/js/switch.js
@@ -5,26 +5,35 @@ function page_swit(a)
 	case "home":
 	{
 		document.getElementById("ifra").src = "home.html";
-		document.getElementByClassName("switch").style.border = "0px solid red";
+		hide_border();
 		document.getElementById("home").style.border = "2px solid red";
 	}
 	break;
 	case "cv":
 	{
 		document.getElementById("ifra").src = "cv.html";
-		document.getElementByClassName("switch").style.border = "0px solid red";
+		hide_border();
 		document.getElementById("cv").style.border = "2px solid red";
 	}
 	break;
 	case "publ":
 	{
 		document.getElementById("ifra").src = "publication.html";
-		document.getElementByClassName("switch").style.border = "0px solid red";
+		hide_border()
 		document.getElementById("publ").style.border = "2px solid red";
 	}
 	break;
 	default:
-		document.getElementById("ifra").src = "home.html";
+		;
 	}
 	document.getElementById("asd").value = a;
+}
+
+function hide_border()
+{
+var list=document.getElementById("menu");
+var str=list.getElementsByTagName("td");
+for(var i=0;i<str.length;i++)
+	if(str[i].className=="switch")
+		str[i].style.border = "0px solid red";   
 }
