c3f04d62e92e7f0fa592f2b100f24bf16bd11de6
Manfredo <wangzhiyu0471@hust.edu.cn>
2017-07-26 19:52:58 +0800

    js added

---
+++ js/switch.js
@@ -0,0 +1,25 @@
+function page_swit(para){
+	switch (para){
+		case "home":{
+			document.getElementById("ifra").src = "home.html";
+			break;
+		}
+		case "note":{
+			document.getElementById("ifra").src = "note/note.html";
+			break;
+		}
+		case "program":{
+			document.getElementById("ifra").src = "prog/program.html";
+			break;
+		}
+		case "scitific_research":{
+			document.getElementById("ifra").src = "scitificr/index.html";
+			break;
+		}
+		case "resume":{
+			document.getElementById("ifra").src = "resume/resume.html";
+			break;
+		}
+		
+	}
+}
